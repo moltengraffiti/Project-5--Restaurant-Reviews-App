f3430629c69b1446e42f2170fa1ff8b3238d6e59
Sam Reilly <samreilly@gmail.com>
2019-02-11 14:55:07 +1100

    Done: API Key, flex on filter list

--- js/main.js
+++ js/main.js
@@ -78,7 +78,7 @@ initMap = () => {
         scrollWheelZoom: false
       });
   L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.jpg70?access_token={mapboxToken}', {
-    mapboxToken: '<your MAPBOX API KEY HERE>',
+    mapboxToken: 'pk.eyJ1IjoibW9sdGVuZyIsImEiOiJjanJ6bjBzbDMxYWhvNDRvM2s2OGJpamE4In0._AEvzFcU3_NwNpoSh84oFg',
     maxZoom: 18,
     attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, ' +
       '<a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
