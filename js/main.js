3631fcc6dc7516f080d2a24321cc936234ea3d43
Sam Reilly <samreilly@gmail.com>
2019-02-12 17:24:22 +1100

    Updated card h2 to h3, filter options styling

--- js/main.js
+++ js/main.js
@@ -163,8 +163,8 @@ createRestaurantHTML = (restaurant) => {
   image.src = DBHelper.imageUrlForRestaurant(restaurant);
   li.append(image);
 
-  /* Change to h2 */
-  const name = document.createElement('h2');
+  /* Change to h3 */
+  const name = document.createElement('h3');
   name.innerHTML = restaurant.name;
   li.append(name);
 
