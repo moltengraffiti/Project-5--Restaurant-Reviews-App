42b39a569d58ba0fd877f9a71cbce53c112f740e
Sam Reilly <samreilly@gmail.com>
2019-02-17 13:27:56 +1100

    building out sw

--- sw.js
+++ sw.js
@@ -1,5 +1,24 @@
 
+
+const cacheName='restaurant-v1';
+const cacheAssets=[
+    'index.html',
+    'restaurant.html',
+    '/css/main.css',
+    '/css/responsive.css',
+    '/js/dbhelper.js',
+    '/js/main.js',
+    '/js/restaurant_info.js',
+    '/img/*',
+    '/js/register.js'
+];
+
 //Listen for the instalation event fired when service worker is registered
 self.addEventListener('install', function(e){
+    e.waitUntil(
+        caches.open('restaurant-v1').then(function(cache){
+            return cache.addAll(cacheAssets);
+        })
+    );
 
 });
