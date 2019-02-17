97eaf2ee2df305c7e316655b72b49a0300bf8587
Sam Reilly <samreilly@gmail.com>
2019-02-17 14:17:15 +1100

    Added new fetch request to cache

--- sw.js
+++ sw.js
@@ -1,7 +1,7 @@
 
 
-const cacheName='restaurant-v1';
-const cacheAssets=[
+const cacheName = 'restaurant-v1';
+const cacheAssets = [
     '/',
     '/index.html',
     '/restaurant.html',
@@ -12,14 +12,14 @@ const cacheAssets=[
     'js/dbhelper.js',
     'js/main.js',
     'js/restaurant_info.js',
-   
+
 
 ];
 
-//Listen for the instalation event fired when service worker is registered
-self.addEventListener('install', function(event){
+//Listen for the instalation event fired when service worker is registered. then add the cache assets
+self.addEventListener('install', function (event) {
     event.waitUntil(
-        caches.open(cacheName).then(function(cache){
+        caches.open(cacheName).then(function (cache) {
             return cache.addAll(cacheAssets);
         })
     );
@@ -27,13 +27,13 @@ self.addEventListener('install', function(event){
 });
 
 
-
-self.addEventListener('activate', function(event){
+//Remove old cache 
+self.addEventListener('activate', function (event) {
     event.waitUntil(
-        caches.keys().then(function(cacheNames){
+        caches.keys().then(function (cacheNames) {
             return Promise.all(
-                cacheNames.map(function(cacheName){
-                    if(cacheName.startsWith('restaurant-v1') && staticCacheName!=cacheName){
+                cacheNames.map(function (cacheName) {
+                    if (cacheName.startsWith('restaurant-v1') && staticCacheName != cacheName) {
                         return caches.delete(cacheName);
                     }
                 })
@@ -42,15 +42,23 @@ self.addEventListener('activate', function(event){
     );
 });
 
-self.addEventListener('fetch', function(event){
-event.respondWith(
-    caches.match(e.request).then(function(response){
-if(response){
-    return response;
-}
-else{
-    return fetch(event.request);
-}
-    })
-);
+//See if the fetch url is already cached, if yes, return it. If not, fetch it and add it to the cache
+self.addEventListener('fetch', function (event) {
+    event.respondWith(
+        caches.match(event.request).then(function (response) {
+            if (response) {
+                return response;
+            }
+            else {
+                return fetch(event.request).then(function (response) {
+                    const clonedResponse=response.clone();
+                    caches.open('restaurant-v1').then(function(cache){
+                        cache.put(event.request, clonedResponse);
+                    })
+                    return response;
+                })
+         
+            }
+        })
+    );
 });
