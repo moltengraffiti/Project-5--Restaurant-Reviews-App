fce6de944a7591f6a2914770924aa7220adb921d
Sam Reilly <samreilly@gmail.com>
2019-02-17 13:47:50 +1100

    Cache working with two tests

--- sw.js
+++ sw.js
@@ -2,23 +2,46 @@
 
 const cacheName='restaurant-v1';
 const cacheAssets=[
+    '/',
     'index.html',
-    'restaurant.html',
-    '/css/main.css',
-    '/css/responsive.css',
-    '/js/dbhelper.js',
-    '/js/main.js',
-    '/js/restaurant_info.js',
-    '/img/*',
-    '/js/register.js'
+
 ];
 
 //Listen for the instalation event fired when service worker is registered
-self.addEventListener('install', function(e){
-    e.waitUntil(
-        caches.open('restaurant-v1').then(function(cache){
+self.addEventListener('install', function(event){
+    event.waitUntil(
+        caches.open(cacheName).then(function(cache){
             return cache.addAll(cacheAssets);
         })
     );
 
+});
+
+
+
+self.addEventListener('activate', function(event){
+    event.waitUntil(
+        caches.keys().then(function(cacheNames){
+            return Promise.all(
+                cacheNames.map(function(cacheName){
+                    if(cacheName.startsWith('restaurant-v1') && staticCacheName!=cacheName){
+                        return caches.delete(cacheName);
+                    }
+                })
+            );
+        })
+    );
+});
+
+self.addEventListener('fetch', function(event){
+event.respondWith(
+    caches.match(e.request).then(function(response){
+if(response){
+    return response;
+}
+else{
+    return fetch(event.request);
+}
+    })
+);
 });
